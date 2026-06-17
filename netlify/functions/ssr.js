d739fea9d9eb4f22d32feaa3994c823af8548ee4
Ramy-Trans <465468415@cairo3.moe.edu.eg>
2026-06-17 17:39:29 +0300

    chore: remove stale Netlify SSR function (no longer used)

--- netlify/functions/ssr.js
+++
@@ -1,7 +0,0 @@
-import handler from "./server-bundle.js";
-
-export default async (request, context) => {
-  return handler.fetch(request, process.env, context);
-};
-
-export const config = { path: "/*" };
